eaaee3e3e78a4ba08c8bde3aefea646e634abb63
Valera <valerylarionov06@gmail.com>
2024-01-27 23:01:58 +0300

    choice

--- choice.py
+++ choice.py
@@ -4,10 +4,11 @@ import argparse
 def main():
     """
     choices нужен, чтобы можно было вводить значение только из указанного в choices
+    type должен соответствовать типу данных в choices
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('arg_choice', choices=(range(1, 101)), type=int)
+    parser.add_argument('arg_choice', choices=[1, 2, 3, 4, 5], type=int)
     args = parser.parse_args()
 
     print(f'вы мне должны {args.arg_choice}k рублей')
@@ -16,4 +17,4 @@ def main():
 main()
 
 # python choice.py 1
-# python choice.py 0
+# python choice.py 5
